087f406025ae2d62c004803ff9a15a937cbacc91
Evgeniy T <e737business@gmail.com>
2024-09-17 13:30:07 +0700

    removed functions

--- steam.py
+++ steam.py
@@ -24,25 +24,21 @@ def driver():
     driver.quit()
 
 
-def main_page(driver):
-    login_link = WebDriverWait(driver, TIMEOUT).until(
-        ec.element_to_be_clickable(INSTALL)
-    )
+def test(driver):
+    WebDriverWait(driver, TIMEOUT).until(
+        ec.element_to_be_clickable(INSTALL))
 
-
-def login_page(driver):
     login_link = WebDriverWait(driver, TIMEOUT).until(
         ec.element_to_be_clickable(LOG_IN)
     )
 
     login_link.click()
 
-    WebDriverWait(driver, TIMEOUT).until(
-        ec.url_contains("login")
+    username_elem = WebDriverWait(driver, TIMEOUT).until(
+        ec.element_to_be_clickable(USERNAME)
     )
+    assert username_elem.is_displayed(), 'Login page was not opened'
 
-
-def input_creds(driver):
     faker = Faker()
     login_input = WebDriverWait(driver, TIMEOUT).until(
         ec.element_to_be_clickable(USERNAME))
@@ -57,30 +53,18 @@ def input_creds(driver):
         ec.element_to_be_clickable(SUBMIT))
     press_button.click()
 
-
-def loading_element(driver):
     loading_elem = WebDriverWait(driver, TIMEOUT).until(
         ec.presence_of_element_located(LOADING)
 
     )
     assert loading_elem.is_displayed(), "Элемент загрузки не отображается"
 
-
-def check_error_message(driver):
     WebDriverWait(driver, TIMEOUT).until(
         ec.invisibility_of_element_located(LOADING)
 
     )
 
-    error_message = WebDriverWait(driver, 10).until(
+    error_message = WebDriverWait(driver, TIMEOUT).until(
         ec.presence_of_element_located(ERROR)
     )
     assert error_message.is_displayed(), "Сообщение об ошибке не отображается"
-
-
-def test_login(driver):
-    main_page(driver)
-    login_page(driver)
-    input_creds(driver)
-    loading_element(driver)
-    check_error_message(driver)
